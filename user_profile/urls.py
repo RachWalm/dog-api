8524fc19363420aadf39e6a687914989033c9a6a
Rachel Walmsley <rachwalm75@gmail.com>
2024-04-12 10:25:12 +0100

    Feat. Add view and update userprofile functionality

--- user_profile/urls.py
+++ user_profile/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from user_profile import views
 
 urlpatterns = [
-    path ('user_profile/', views.UserProfileList.as_view()),
+    path('user_profile/', views.UserProfileList.as_view()),
+    path('user_profile/<int:pk>/', views.UserProfileDetail.as_view())
 ]
--- user_profile/views.py
+++ user_profile/views.py
@@ -1,3 +1,5 @@
+from django.http import Http404
+from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import UserProfile
@@ -8,3 +10,25 @@ class UserProfileList(APIView):
         user_profiles = UserProfile.objects.all()
         serializer = UserProfileSerializer(user_profiles, many=True)
         return Response(serializer.data)
+    
+class UserProfileDetail(APIView):
+    serializer_class = UserProfileSerializer
+    def get_object(self, pk):
+        try:
+            user_profile = UserProfile.objects.get(pk=pk)
+            return user_profile
+        except UserProfile.DoesNotExist:
+            raise Http404
+        
+    def get(self, request, pk):
+        user_profile = self.get_object(pk)
+        serializer = UserProfileSerializer(user_profile)
+        return Response(serializer.data)
+    
+    def put(self, request, pk):
+        user_profile =  self.get_object(pk)
+        serializer = UserProfileSerializer(user_profile, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
