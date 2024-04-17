611caabb552092b494e6126d435ac3de9983fddb
Rachel Walmsley <rachwalm75@gmail.com>
2024-04-17 10:40:23 +0100

    Fix DogProfile not saving as missing null's and typo in settings

--- dog_profile/views.py
+++ dog_profile/views.py
@@ -17,8 +17,8 @@ class DogProfileList(generics.ListCreateAPIView):
     #     'post'
     # ]
     
-    def perform_create(self, serializer):
-        serializer.save(user_id=self.request.user)
+    # def perform_create(self, serializer):
+    #     serializer.save(dog_id=self.request.user)
         
 
 class DogProfileDetail(generics.RetrieveUpdateDestroyAPIView):
--- posts/serializers.py
+++ posts/serializers.py
@@ -17,5 +17,5 @@ class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
         fields= [
-            'id', 'user_id', 'created_at', 'updated_at', 'title', 'content', 'image', 'is_owner', 'users_first_name'
+            'id', 'user_id', 'created_at', 'updated_at', 'title', 'content', 'image', 'is_owner', 'users_first_name', 'dog_id'
         ]
