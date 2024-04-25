59c81b02504e6c5ac7446fcce78031a9aee23cf9
Rachel Walmsley <rachwalm75@gmail.com>
2024-04-25 10:52:12 +0100

    Feat. Add filtering and sorting to dogvaccine

--- dog_vaccine/views.py
+++ dog_vaccine/views.py
@@ -1,5 +1,5 @@
-from rest_framework import generics, permissions
-# from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import generics, permissions, filters
+from django_filters.rest_framework import DjangoFilterBackend
 from dog_api.permissions import IsSuperUser
 from .models import DogVaccine
 from .serializers import DogVaccineSerializer
@@ -8,14 +8,25 @@ class DogVaccineList(generics.ListAPIView):
     permission_classes = [IsSuperUser]
     serializer_class =  DogVaccineSerializer
     queryset = DogVaccine.objects.all()
-    # filter_backends = [
-    #     DjangoFilterBackend
-    # ]
-    
-    # filterset_fields = [
-    #     # all comments associated with a post
-    #     'post'
-    # ]
+    filter_backends = [
+        DjangoFilterBackend,
+        filters.OrderingFilter,
+    ]
+    ordering_fields = [
+        'vaccine_canine_parvovirus',
+        'vaccine_canine_hepatitis',
+        'vaccine_distemper',
+        'vaccine_leptospirosis',
+        'vaccine_kennelcough',
+        'vaccine_rabies',
+        'vaccine_puppy_first',
+        'vaccine_puppy_second',
+        'vaccine_sixmonthboost',
+        'vaccine_twelvemonthboost',
+    ]
+    filterset_fields = [
+        'overdue',
+    ]
 
 class DogVaccineDetail(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = [IsSuperUser]
