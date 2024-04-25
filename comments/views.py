142e3a626cd1fad58e8e60c036fca55dc4c4aaa7
Rachel Walmsley <rachwalm75@gmail.com>
2024-04-25 11:24:12 +0100

    Feat. Add comments filtering/search/ordering

--- comments/views.py
+++ comments/views.py
@@ -1,5 +1,5 @@
-from rest_framework import generics, permissions
-# from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import generics, permissions, filters
+from django_filters.rest_framework import DjangoFilterBackend
 from dog_api.permissions import IsOwnerOrReadOnly
 from .models import Comment
 from .serializers import CommentSerializer, CommentDetailSerializer
@@ -8,14 +8,21 @@ class CommentList(generics.ListCreateAPIView):
     serializer_class =  CommentSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     queryset = Comment.objects.all()
-    # filter_backends = [
-    #     DjangoFilterBackend
-    # ]
-    
-    # filterset_fields = [
-    #     # all comments associated with a post
-    #     'post'
-    # ]
+    filter_backends = [
+        filters.OrderingFilter,
+        filters.SearchFilter,
+        DjangoFilterBackend,
+    ]
+    ordering_fields = [
+        'updated_at',
+        'created_at'
+    ]
+    search_fields = [
+        'comment_content',
+    ]
+    filterset_fields = [
+        'user_id',
+    ]
     
     def perform_create(self, serializer):
         serializer.save(user_id=self.request.user)
