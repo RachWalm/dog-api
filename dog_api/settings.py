67072eeacd0babe1ff52c1ac0cd131c031b1ab83
Rachel Walmsley <rachwalm75@gmail.com>
2024-04-29 15:28:29 +0100

    Feat production app -> JSON not html, moved time/date code up settings.py

--- dog_api/settings.py
+++ dog_api/settings.py
@@ -31,8 +31,17 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.SessionAuthentication'
         if 'DEV' in os.environ
         else 'dj_rest_auth.jwt_auth.JWTCookieAuthentication'
-    )]
+    )],
+     'DEFAULT_PAGINATION_CLASS':
+        'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
+    'DATETIME_FORMAT': "%A %d %b %Y %H:%M:%S",
+    'DATE_FORMAT' : "%A %d %b %Y",
 }
+if 'DEV' not in os.environ:
+    REST_FRAMEWORK['DEFAULT_RENDERER_CLASSES'] = [
+        'rest_framework.renderers.JSONRenderer',
+    ]
 
 REST_USE_JWT = True
 JWT_AUTH_SECURE = True
@@ -159,14 +168,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
-# Date and Time format
-
-REST_FRAMEWORK = {
-
-    'DATETIME_FORMAT': "%A %d %b %Y %H:%M:%S",
-    'DATE_FORMAT' : "%A %d %b %Y",
-}
 
 
 # Static files (CSS, JavaScript, Images)
