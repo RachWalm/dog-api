bc6040b2aa967616ab1a03014a65e5a40171a1cf
Rachel Walmsley <rachwalm75@gmail.com>
2024-05-01 12:00:35 +0000

    Return settings.py CORS code to previous state

--- dog_api/settings.py
+++ dog_api/settings.py
@@ -70,20 +70,16 @@ ALLOWED_HOSTS = [
    '8000-rachwalm-dogapi-yx7u8zwd81j.ws-eu110.gitpod.io'
 ]
 
-# if 'CLIENT_ORIGIN' in os.environ:
-#     CORS_ALLOWED_ORIGIN = [
-#         os.environ.get('CLIENT_ORIGIN')
-#     ]
+if 'CLIENT_ORIGIN' in os.environ:
+    CORS_ALLOWED_ORIGIN = [
+        os.environ.get('CLIENT_ORIGIN')
+    ]
 
-# if 'CLIENT_ORIGIN_DEV' in os.environ:
-#     extracted_url = re.match(r'^.+-', os.environ.get('CLIENT_ORIGIN_DEV', ''), re.IGNORECASE).group(0)
-#     CORS_ALLOWED_ORIGIN_REGEXES = [
-#         rf"{extracted_url}(eu|us)\d+\w\.gitpod\.io$",
-#     ]
-
-CORS_ALLOWED_ORIGIN_REGEXES = [
-    r'^https://.*\.gitpod\.io$',
-]
+if 'CLIENT_ORIGIN_DEV' in os.environ:
+    extracted_url = re.match(r'^.+-', os.environ.get('CLIENT_ORIGIN_DEV', ''), re.IGNORECASE).group(0)
+    CORS_ALLOWED_ORIGIN_REGEXES = [
+        rf"{extracted_url}(eu|us)\d+\w\.gitpod\.io$",
+    ]
 
 CORS_ALLOW_CREDENTIALS = True
 
