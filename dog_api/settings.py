5e4501e6de93d817dcd96f4013031e2d409749ca
Rachel Walmsley <rachwalm75@gmail.com>
2024-05-01 11:01:41 +0100

    Fix Add CORS_ALLOW_CREDENTIALS = True

--- dog_api/settings.py
+++ dog_api/settings.py
@@ -70,7 +70,7 @@ ALLOWED_HOSTS = [
 ]
 
 if 'CLIENT_ORIGIN' in os.environ:
-    CORS_ALLOWED_ORIGIN = [
+    CORS_ALLOWED_ORIGINS = [
         os.environ.get('CLIENT_ORIGIN')
     ]
 
@@ -79,6 +79,8 @@ if 'CLIENT_ORIGIN_DEV' in os.environ:
     CORS_ALLOWED_ORIGIN_REGEXES = [
         rf"{extracted_url}(eu|us)\d+\w\.gitpod\.io$",
     ]
+    
+CORS_ALLOW_CREDENTIALS = True
 
 # Application definition
 
